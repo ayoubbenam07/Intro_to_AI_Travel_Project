cc4d0e054294e77c098bff2de550053d949aa0d4
Rayane <maaroufrayane3@gmail.com>
2026-05-17 03:36:20 +0100

    pretty changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import Map from "./map/Map"
 import Home from "./home/Home"
 import FooterSection from "./footer/Footer";
 import Navbar from "./navbar/Navbar";
-
+import PlanJourney from "./plan/PlanJourney";
 
 export default function App() {
 
   return <>
     <Navbar />
-    <Home />
+    {/* <Home /> */}
+    <PlanJourney />
     <FooterSection />
   </>;
 }
